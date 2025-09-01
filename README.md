4dc12f4adfb41cd3aeca2dcf9a1aa5cc942bc636
farmani1 <f.armani1@campus.unimib.it>
2025-09-01 09:17:51 +0200

    Archiving: Moving from local to remote storage (upstash-redis)

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ pip install -r requirements.txt
 2. Go to the [Gemini API website](https://aistudio.google.com/app/apikey)
 3. Generate a new API key.
 4. Copy the generated key and paste it into a file named `api_key.txt`. Optionally, you can run `main.py` with the `-api` flag if you want to store the key.
-5. Make sure you are connected to your university's VPN — this is required to access the Elasticsearch database.
-6. Start the graph. Run the following command:
+5. Insert an upstash-redis URL and token into the `url_redis.txt` and `token_redis.txt` files in the settings directory (create the files if necessary)
+6. Make sure you are connected to your university's VPN — this is required to access the Elasticsearch database.
+7. Start the graph. Run the following command:
 ```bash
 python rewriting_system/main.py
 ```
-7. Run the `main.py` file. Use the `-rag` flag if you want only relevant chunks to be used during retrieval.
+8. Run the `main.py` file. Use the `-rag` flag if you want only relevant chunks to be used during retrieval.
 ```bash
 python main.py
 ```
--- rewriting_system/storage.py
+++ rewriting_system/storage.py
@@ -1,30 +1,59 @@
-import redis
+from upstash_redis import Redis
 import json
-from datetime import datetime
+import os
+from datetime import datetime, timezone
 from typing import List, Dict, Any, Optional
+
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
+from utils.file_manager import read_file
 
 class Storage:
     def __init__(self):
-        self.r = {}
+        self.r = Redis(url=read_file(os.path.join(project_root, "settings", "url_redis.txt")), 
+                       token=read_file(os.path.join(project_root, "settings", "token_redis.txt")))
 
     def read(self, key: str) -> List[Dict[str, Any]]:
-        if not (key in self.r.keys()):
+        data = self.r.get(key)
+        
+        if data is None:
             return []
         
-        return self.r.get(key)
-
+        return json.loads(data)
+    
     def write(self, key: str, query: str, docRef: str, data: Optional[List[Dict[str, Any]]] = None):
         if data is None:
             data = self.read(key)
 
-        data.append({
-            "time": datetime.now().isoformat(),
+        now = datetime.now(timezone.utc)
+
+        filtered = []
+        for d in data:
+            t = datetime.fromisoformat(d["time"])
+            
+            if t.tzinfo is None:
+                t = t.replace(tzinfo=timezone.utc)
+            
+            if int(abs((now - t).total_seconds() / 60)) < 60:
+                filtered.append(d)
+        
+        id_doc = "0"
+        if len(filtered) > 0:
+            last_doc_id = filtered[-1]["docOut"]
+            id_doc = str(int(last_doc_id[last_doc_id.index("_") + 1:]) + 1)
+            
+        filtered.append({
+            "time": now.isoformat(),
             "query": query,
             "docRef": docRef,
-            "docOut": f"doc_{len(data)}"
+            "docOut": f"doc_{id_doc}"
         })
 
-        self.r.update({key: data})
+        self.r.set(key, json.dumps(filtered), ex=1800)
 
     def clear(self, key: str):
-        self.r.update({key: []})
+        self.r.set(key, json.dumps([]))
