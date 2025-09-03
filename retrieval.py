52050bad34e7a3828beef87595138976ea542ef8
farmani1 <f.armani1@campus.unimib.it>
2025-09-04 00:48:14 +0200

    Retrieval reorganization: management via dict lists and local reading (temporary - due to not "needing" the VPN)

--- retrieval.py
+++ retrieval.py
@@ -1,31 +1,29 @@
 from elasticsearch import Elasticsearch
 
 from utils.config import Config
+from utils.file_manager import read_file
+
+import os
 
 class Retrieval:
     def __init__(self, cfg: Config):
         self.client = Elasticsearch(cfg.DB_URL)
         
-        # TO-DO Update with a dynamic system / annotations
+        #self.rag = cfg.rag
+        self.rag = False
         
-        self.docs = {
-            "sentenza di primo grado": "S1 - AN.docx",
-            "sentenza di secondo grado": "S2  - AN.docx",
-            "memoria giudiziale": "M2  - AN.docx",
-            "ricorso giudiziale": "R2 - AN.docx"
-        }
-        
-        self.rag = cfg.rag
-        
-    def execute(self, operation: dict) -> tuple[str,str]:
+    def execute(self, operation: dict) -> list[dict]:
         if self.rag:
             
             # TO-DO only-chunk retrieval
             
             print("to-do")
-            return ("", "")
+            return [{
+                "name": "",
+                "text": ""
+            }]
         else: 
-            db_query = {
+            '''db_query = {
                 "query": {
                     "bool": {
                         "must": [
@@ -43,4 +41,20 @@ class Retrieval:
                 if "name" in hit["_source"].keys() and "text" in hit["_source"].keys():
                     return (hit["_source"]["name"], hit["_source"]["text"])
             
-            return ("", "")
+            return ("", "")'''
+            
+            docs = []
+            for d in operation["documents"]:
+                path_file = os.path.join("documents", f"{d}.json")
+                
+                if os.path.exists(path_file):
+                    file = read_file(path_file)
+                    
+                    docs.append({
+                        "name": d,
+                        "text": file["text"]
+                    })
+                else:
+                    print("File not Found")
+            
+            return docs
